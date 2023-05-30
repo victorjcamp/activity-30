d289b71fdd5de2676b63702317e10ac948d928f3
victorjcamp <125593544+victorjcamp@users.noreply.github.com>
2023-05-30 08:10:51 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+// Select color input
+// Select size input
+var height, width, color;
+
+// When size is submitted by the user, call makeGrid()
+$('#sizePicker').submit(function (event){
+	event.preventDefault();
+	height = $('#inputHeight').val();
+	width = $('#inputWeight').val();
+	makeGrid(height, width);
+//console.log('height:' + height);
+});
+
+function makeGrid(x, y) {
+	$('tr').remove();
+
+// Your code goes here!
+	for(var i = 1; i <= x; i++){
+		$('#pixelCanvas').append('<tr id=table' + i + '></tr>');
+		for(var j = 1; j <= y; j++){
+			$('#table' + i).append('<td></td>');
+		}
+	}
+
+	// When color is selected by the user, add color to grid
+	$('td').click(function addColor(){
+		color = $('#colorPicker').val();
+		if ($(this).attr('style')){
+			$(this).removeAttr('style');
+		} else{
+			$(this).attr('style','background-color:' + color);
+		}
+				
+	});
+	
+}
